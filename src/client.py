238854fb6e7fc06eb33c66c30d66593a4521d8cd
Yoel Enriquez Sena <71104727+elex373@users.noreply.github.com>
2024-04-09 17:14:48 -0400

    add: ACCT

--- src/client.py
+++ src/client.py
@@ -59,6 +59,16 @@ class FTPClient:
         self.ftp_socket.sendall(f"{message}\r\n".encode())
         return self.response()
     
+    def server_info(self, path):
+        if path is None:
+            response = self.send("STAT ")
+            response += self.response()
+            print(response)
+        else:
+            response = self.send("STAT {path}")
+            response += self.response()
+            print(response)
+
     def default_login(self):
 
         self.send(f"USER anonymous\r\n")
@@ -475,6 +485,11 @@ if __name__ == "__main__":
                 client.size(args[0])
             else:
                 print("Error: size requiere un argumento.")
+        elif command == "server-info":
+            if len(args) > 0:
+                client.server_info(args[0])
+            else:
+                client.server_info(None)
         elif command == "quit" or command == "exit":
             client.close()
             break
